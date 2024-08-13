fed1b2ce4a255d461b2a81569534e8803158b618
Brian Schlining <bschlining@gmail.com>
2024-08-13 13:58:46 -0700

    added release workflow

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,17 +10,17 @@ object Dependencies {
   lazy val jansi    = "org.fusesource.jansi"         % "jansi"           % "2.4.1"
   lazy val logback  = "ch.qos.logback"               % "logback-classic" % "1.5.6"
   lazy val methanol = "com.github.mizosoft.methanol" % "methanol"        % "1.7.0"
-  lazy val munit    = "org.scalameta"               %% "munit"           % "1.0.0"
+  lazy val munit    = "org.scalameta"               %% "munit"           % "1.0.1"
   lazy val picocli  = "info.picocli"                 % "picocli"         % "4.7.6"
 
-  lazy val slf4jVersion = "2.0.13"
+  lazy val slf4jVersion = "2.0.16"
   lazy val slf4jApi     = "org.slf4j"      % "slf4j-api"                    % slf4jVersion
   lazy val slf4jJul     = "org.slf4j"      % "jul-to-slf4j"                 % slf4jVersion
   lazy val slf4jJdk     = "org.slf4j"      % "slf4j-jdk-platform-logging"   % slf4jVersion
 
   lazy val sttpCirceClient = "com.softwaremill.sttp.client3" %% "circe" % "3.9.5"
 
-  private val tapirVersion  = "1.10.14"
+  private val tapirVersion  = "1.11.1"
   lazy val tapirStubServer  = "com.softwaremill.sttp.tapir" %% "tapir-sttp-stub-server" % tapirVersion
   lazy val tapirSwagger     = "com.softwaremill.sttp.tapir" %% "tapir-swagger-ui-bundle" % tapirVersion
   lazy val tapirCirce       = "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % tapirVersion
@@ -28,6 +28,6 @@ object Dependencies {
   lazy val tapirVertx       = "com.softwaremill.sttp.tapir" %% "tapir-vertx-server" % tapirVersion
 
   lazy val typesafeConfig = "com.typesafe"   % "config"          % "1.4.3"
-  lazy val zio            = "dev.zio"       %% "zio"             % "2.1.6"
+  lazy val zio            = "dev.zio"       %% "zio"             % "2.1.7"
   
 }
